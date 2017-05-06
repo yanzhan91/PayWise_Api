fb083790c710e1a057573a89780f134c5d37162d
yanzhan91 <yanzhan91@gmail.com>
2017-05-05 21:33:46 -0500

    Card name string matching added

--- scripts/PayWise_User_Card_Post.py
+++ scripts/PayWise_User_Card_Post.py
@@ -1,5 +1,6 @@
 import boto3
 from boto3.dynamodb.conditions import Key
+import difflib
 
 
 def handler(event, context):
@@ -20,14 +21,28 @@ def start_request(event):
     if 'card_name' not in event:
         raise Exception('Bad Request: No card_name found')
 
-    card_id = get_card_id_from_name(event['card_name'].title())
+    card_name = find_existing_name(event['card_name'].title())
+    card_id = get_card_id_from_name(card_name)
     add_card_id_to_user(event['user_id'], card_id)
 
-    return 'Card added successfully'
+    return card_name
+
+
+def find_existing_name(card_name):
+    card_table = boto3.resource('dynamodb').Table('PayWise_Cards')
+    response = card_table.scan(
+        ProjectionExpression='card_name'
+    )
+    all_cards = list(map(lambda x: x['card_name'], response['Items']))
+    card_names = difflib.get_close_matches(card_name, all_cards, 1)
+    if not card_names:
+        raise Exception('Bad Request: Cannot find card name in database: ' + card_name)
+    return card_names[0]
 
 
 def get_card_id_from_name(card_name):
     card_table = boto3.resource('dynamodb').Table('PayWise_Cards')
+
     response = card_table.query(
         IndexName='card_name-index',
         KeyConditionExpression=Key('card_name').eq(card_name),
@@ -51,3 +66,10 @@ def add_card_id_to_user(user_id, card_id):
     )['ResponseMetadata']
     if response['HTTPStatusCode'] != 200:
         raise Exception('Internal Error: Failed to update Users table')
+
+
+if __name__ == '__main__':
+    print(start_request({
+        'user_id': '10001',
+        'card_name': 'case freedom'
+    }))
